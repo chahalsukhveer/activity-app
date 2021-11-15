4771b899dcf3bf0bab3c0f2cae1084e8e9b4e959
chahalsukhveer <chahal.sukhveer@gmail.com>
2021-11-15 21:43:57 +0100

    Added css for footer

--- src/header/header.js
+++ src/header/header.js
@@ -6,7 +6,7 @@ const Header = ({subtitle}) => {
             <div className="col-md-5">
             <img src={logo} className="logo" alt="logo" />
             </div>
-            <div className="col-md-7 mt-5 subtitle" subtitle>
+            <div className="col-md-7 mt-5 subtitle" >
                    {subtitle}
             </div>
         </div>
