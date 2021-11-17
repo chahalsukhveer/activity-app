52633bbd3544039b0436497da7d52bda3539260f
chahalsukhveer <chahal.sukhveer@gmail.com>
2021-11-17 20:01:19 +0100

    activity list  and useState

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import Header from './header/header';
 import Footer from './footer';
 
 function App() {
+  const activities = [{
+    "name":"Running",
+    "duration":"45 mins",
+    "time":"Monday 7pm"
+   },
+   {
+       "name":"Cycling",
+       "duration":"1 hr",
+       "time":"Tuesday 2pm"
+      },
+      {
+       "name":"Yoga",
+       "duration":"45 mins",
+       "time":"Monday 9am"
+      },
+      {
+        "name":"Meditation",
+        "duration":"45 mins",
+        "time":"Friday 7pm"
+       }]
   return (
     <div className="container">
-        <Header subtitle="Workout for mind & boyy"></Header>
+        <Header subtitle="Workout for mind & body"></Header>
         <div className="row">
-          
         </div>
         <Footer footerText="It is awesome app"></Footer>
     </div>
---
+++ src/activity-list/index.js
@@ -0,0 +1,23 @@
+const ActivityList = () => {
+    return (
+        <>
+        <div className="col-8">
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">Activity</th>
+                        <th scope="col">Duration</th>
+                        <th scope="col">Time</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+
+            </div>
+        </>
+      );
+}
+ 
+export default ActivityList;
